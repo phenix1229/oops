8698854865b2e781c2d5c2f2c45a86bd3512df57
phenix1229 <paul.garay@codeimmersives.com>
2019-11-27 13:01:47 -0500

    half done

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const Stack = function() {
+  return {
+    items: [],
+
+  add: function(str) {
+    this.items.push(str);
+  },
+
+  remove: function() {
+    return this.items.pop();
+  },
+
+  peek: function() {
+    return this.items[this.items.length - 1]
+  },
+  }
 }
+
+
 
 const Queue = function() {
 }
