af0ca03ac22fb70ec5a30bf2766b1c4c9b0d3002
phenix1229 <paul.garay@codeimmersives.com>
2019-11-27 13:27:11 -0500

    complete

--- main.js
+++ main.js
@@ -19,6 +19,21 @@ const Stack = function() {
 
 
 const Queue = function() {
+  return {
+    items: [],
+
+    add: function(str) {
+      this.items.push(str);
+    },
+
+    remove: function() {
+      return this.items.shift();
+    },
+
+    peek: function() {
+      return this.items[0];
+    }
+  }
 }
 
 module.exports = {
